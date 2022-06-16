8f05d210b094768c5f856f3517274503b21ed5b6
alexbashkatov6 <alexbashkatov6@gmail.com>
2022-06-16 16:46:51 +0300

    match-object

--- main.py
+++ main.py
@@ -4,4 +4,14 @@
 
 import re
 
+match = re.match(r"123", "123")
+print(match)
+
+print("group0 = ", match.group(0), match[0])  # i=0 - regex itself
+print("groups = ", match.groups())
+print("groupdict = ", match.groupdict())
+
+print("start = ", match.start(0))
+print("end = ", match.end(0))
+print("span = ", match.span(0))
 
