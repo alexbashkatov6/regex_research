7bb13c65abf5ccf875d9ca55d09627cafcef9f97
alexbashkatov6 <alexbashkatov6@gmail.com>
2022-06-17 17:54:05 +0300

    lesson 3.2-3.4

--- main.py
+++ main.py
@@ -4,14 +4,27 @@
 
 import re
 
-match = re.match(r"123", "123")
-print(match)
+lesson = 3.4
 
-print("group0 = ", match.group(0), match[0])  # i=0 - regex itself
-print("groups = ", match.groups())
-print("groupdict = ", match.groupdict())
+if lesson == 3.2:
+    match = re.match(r"123", "123")
+    print(match)
 
-print("start = ", match.start(0))
-print("end = ", match.end(0))
-print("span = ", match.span(0))
+    print("group0 = ", match.group(0), match[0])  # i=0 - regex itself
+    print("groups = ", match.groups())
+    print("groupdict = ", match.groupdict())
+
+    print("start = ", match.start(0))
+    print("end = ", match.end(0))
+    print("span = ", match.span(0))
+
+if lesson == 3.3:
+    """ 1 match only at string start """
+    match = re.match(r"123", "sdfsdf123")
+    print(match)
+
+if lesson == 3.4:
+    """ 1 match everywere """
+    print(re.search(r"123", "sdfsdf123"))
+    print(re.search(r"123", "sdfsdf13"))
 
