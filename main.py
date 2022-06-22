b89444ad50cce0fd1dde57530a4bc1cd8731d4ba
alexbashkatov6 <alexbashkatov6@gmail.com>
2022-06-22 17:58:01 +0300

    links added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # https://stepik.org/lesson/694358 - курс
+# https://regex101.com/ - site for testing regex
+# https://textcompare.ru/app - site for text comparison
 # r"[а-яА-ЯёЁa-zA-Z]" - только буквы
 # подача неск строк на вход:  str_list = list(map(str.strip, sys.stdin.readlines()))
 # подача 1 строки на вход: s = sys.stdin.read()
